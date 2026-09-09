591063b06f1d16efa46ce771e14d4a53b7e6d6dc
nadila348 <n56995417@gmail.com>
2026-09-09 10:41:37 +0700

    Add files via upload

---
+++ try1.py
@@ -0,0 +1,11 @@
+nama= "nadhila Nurul hodhijah"
+hobi="makeup"
+umur= 16
+perempuan= "true"
+beratbadan= 50
+print("====Biodata====")
+print("nama=",nama)
+print("hobi=",hobi)
+print("umur=",umur)
+print("perempuan=",perempuan)
+print("beratbadan=", beratbadan)
