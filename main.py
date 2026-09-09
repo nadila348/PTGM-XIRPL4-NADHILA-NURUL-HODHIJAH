bdf8ae8ca3caad3085cbba44db8c93e9332d49e4
nadila348 <n56995417@gmail.com>
2026-09-09 12:11:14 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+A = 108
+B = 183
+C = 41
+
+print("1. A & C =", A & C)
+print("2. B | A =", B | A)
+print("3. C ^ B =", C ^ B)
+print("4. A ^ B | C =", A ^ B | C)
+print("5. C ^ A ^ B =", C ^ A ^ B)
